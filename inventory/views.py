ec2fb78f9d581cdea57679dcaa1840fc48534d24
Nguyen Minh Duc <minhduc.xd.93@gmail.com>
2017-03-15 20:28:31 +0000

    first view

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse('<p>In index View</p>')
+
+def item_detail(request,id):
+	response =  HttpResponse()
+	response.write('<p>In item_detail view with id {0}</p>'.format(id))
+	#return HttpResponse(id)
+	return response
+
 
 # Create your views here.
