d1701de28bb5f6c430750253abeebb9068718f05
Nguyen Minh Duc <minhduc.xd.93@gmail.com>
2017-03-19 11:28:25 +0000

    empty project

--- inventory/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Item 
-class itemAdmin(admin.ModelAdmin):
-	list_display = ['title','amount','description']
-
-admin.site.register(Item, itemAdmin)
