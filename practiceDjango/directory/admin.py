611081f6eb3eb1f8fc96bbb6cfd350d0645f96e9
Nguyen Minh Duc <minhduc.xd.93@gmail.com>
2017-03-21 00:16:54 +0000

    apply admin, and custumize admin view

--- practiceDjango/directory/admin.py
+++ practiceDjango/directory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Item 
+
+class itemAdmin(admin.ModelAdmin):
+	list_display = ['title','amount'] 
+
+admin.site.register(Item, itemAdmin)
